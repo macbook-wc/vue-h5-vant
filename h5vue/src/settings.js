0a884756be23dbff2c64ed53e7fe5daacbd5ffd2
王川 <cwang@g2link.cn>
2020-07-28 16:15:51 +0800

    h5骨架

---
+++ h5vue/src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: 'H5Vue',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+  vconsole: true,
+  needPageTrans: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
